2435f3d8947fd3f36f440ee077c31bdd9e2956b7
oshacker <1500438364@qq.com>
2022-07-26 12:37:58 +0800

    购物车从token中获取用户信息，同时对oauth2依赖进行整理

--- xx_service/xx_service_cart/src/main/java/cn/coderap/cart/controller/CartController.java
+++ xx_service/xx_service_cart/src/main/java/cn/coderap/cart/controller/CartController.java
@@ -1,7 +1,7 @@
 package cn.coderap.cart.controller;
 
 import cn.coderap.cart.service.CartService;
-//import cn.coderap.cart.util.TokenDecode;
+import cn.coderap.util.TokenDecode;
 import cn.coderap.constant.StatusCode;
 import cn.coderap.entity.Result;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,7 +16,7 @@ public class CartController {
     @Autowired
     private CartService cartService;
 
-    private String userName = "coderap";
+//    private String userName = "coderap";
 
     /**
      * 添加到购物车分为两种情况：
@@ -28,27 +28,27 @@ public class CartController {
      */
     @GetMapping("/add")
     public Result add(String id, Integer num){
-//        String userName = TokenDecode.getUserInfo().get("username");
+        String userName = TokenDecode.getUserInfo().get("username");
         cartService.add(id, num, userName);
         return new Result(true, StatusCode.OK,"添加成功");
     }
 
     @GetMapping("/list")
     public Map list(){
-//        String userName = TokenDecode.getUserInfo().get("username");
+        String userName = TokenDecode.getUserInfo().get("username");
         return cartService.list(userName);
     }
 
     @DeleteMapping
     public Result delete(@RequestParam(name="skuId") String skuId){
-//        String userName = TokenDecode.getUserInfo().get("username");
+        String userName = TokenDecode.getUserInfo().get("username");
         cartService.delete(skuId,userName);
         return new Result(true,StatusCode.OK,"删除成功");
     }
 
     @PutMapping
     public Result updateChecked(@RequestParam String skuId,@RequestParam Boolean checked){
-//        String userName = TokenDecode.getUserInfo().get("username");
+        String userName = TokenDecode.getUserInfo().get("username");
         cartService.updateCheckedStatus(skuId,checked,userName);
         return new Result(true,StatusCode.OK,"操作成功");
     }
