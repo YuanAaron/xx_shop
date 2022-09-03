bda39d3da9c9ff1b408ce124cdf66eca8b6d7506
oshacker <1500438364@qq.com>
2022-09-03 12:09:12 +0800

    高并发下缓存失效之缓存击穿（修正后的分布式锁）

--- xx_service/xx_service_goods/src/main/java/cn/coderap/goods/service/impl/CategoryServiceImpl.java
+++ xx_service/xx_service_goods/src/main/java/cn/coderap/goods/service/impl/CategoryServiceImpl.java
@@ -12,14 +12,13 @@ import com.github.pagehelper.PageHelper;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.stereotype.Service;
 import org.springframework.util.CollectionUtils;
 import tk.mybatis.mapper.entity.Example;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
@@ -141,30 +140,51 @@ public class CategoryServiceImpl implements CategoryService {
      *    2、等待可以采用自旋的方式
      */
     public Map<String, List<Category2Vo>> getSubCategory2MapWithRedisLock() {
+        //问题1：如果某个线程占到了坑，但业务代码（查数据库）出现异常，可能无法释放锁，导致死锁
+        //解决办法：必须要在finally释放锁
+        //问题2：如果某个线程占到了坑，但是程序在执行过程中宕机，还没有来得及执行释放锁逻辑,就会导致死锁
+        //解决办法：设置锁的自动过期（注意：设置过期时间必须和加锁是原子的，即加锁和设置过期时间必须是一条命令，而不能分开写）
         // 尝试加锁
-        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDISLOCK, "exist");
+        String uuid = UUID.randomUUID().toString();
+        Boolean flag = stringRedisTemplate.opsForValue().setIfAbsent(REDISLOCK, uuid,300, TimeUnit.SECONDS);
         if (flag) {
             // 加锁成功
-            System.out.println("获取分布式锁成功...");
-            // 1、获取锁后，再去缓存中确定一次，如果还没有才查数据库，即双重检验锁
-            String cacheJson = stringRedisTemplate.opsForValue().get(SUBCATJSON);
-            if (StringUtils.isEmpty(cacheJson)) {
-                // 2、查数据库
-                Map<String, List<Category2Vo>> subCategory2MapFromDB = getSubCategory2MapFromDB();
-                System.out.println("查询了数据库...");
+            System.out.println("获取分布式锁成功..." + uuid);
+            try {
+                // 1、获取锁后，再去缓存中确定一次，如果还没有才查数据库，即双重检验锁
+                String cacheJson = stringRedisTemplate.opsForValue().get(SUBCATJSON);
+                if (StringUtils.isEmpty(cacheJson)) {
+                    Map<String, List<Category2Vo>> subCategory2MapFromDB;
+                    // 2、查数据库
+                    subCategory2MapFromDB = getSubCategory2MapFromDB();
+                    System.out.println("查询了数据库..." + uuid);
+                    return subCategory2MapFromDB;
+                }
+                return JSON.parseObject(cacheJson, new TypeReference<Map<String, List<Category2Vo>>>(){});
+            } finally {
+                // 问题3：如果业务代码执行时间较长，锁过期了（其他线程抢占到了锁），这时（业务代码执行完后）我们直接删除锁，可能删除原来自己过期的锁，这无影响，但更坏的情况是有可能删掉别的线程正在持有的锁
+                // 解决办法：占锁时，值指定为uuid，每个人匹配自己的锁时才删除，这样就不会删除掉别人的锁。
                 // 释放锁
-                stringRedisTemplate.delete(REDISLOCK);
-                System.out.println("查询数据库后，分布式锁释放成功...");
-                return subCategory2MapFromDB;
+//                    String value = stringRedisTemplate.opsForValue().get(REDISLOCK); // 1
+//                    if (value.equals(uuid)) { // 2
+//                        // 删除我自己的锁
+//                        stringRedisTemplate.delete(REDISLOCK); // 3
+//                    }
+                // 问题4：假设(lock,xxx)锁10s过期，到此程序执行了9.5s，假设下面的1操作分为两步，第一步请求到达redis花费0.3s，第二步redis响应回来花费0.5（在这个过程中，(lock,xxx)锁过期了，
+                // 另外一个线程加了(lock，yyy)锁），由于响应回来的值是原来的xxx，所以下面的2判断成立，这时再执行下面的3操作就又删除了别的线程正在持有的锁。
+                // 解决办法：与加锁类似，获取值与删除锁不是原子操作，这里可以使用lua脚本（原子操作）来完成
+                // 问题5：如果业务执行时间很长，可能业务还没有执行完，锁先过期了
+                // TODO 解决办法：简单但并不完美的解决方案是将锁过期时间设置的长一些，比如300s；更好的解决方案是执行业务期间，给锁自动续期，比如redisson的实现
+                String script = "if redis.call('get', KEYS[1]) == ARGV[1] then return redis.call('del', KEYS[1]) else return 0 end"; //该lua脚本来自官网http://redis.cn/commands/set.html
+                stringRedisTemplate.execute(new DefaultRedisScript<Long>(script, Long.class), Arrays.asList(REDISLOCK), uuid);
+                System.out.println("分布式锁释放成功..." + uuid);
             }
-            stringRedisTemplate.delete(REDISLOCK);
-            System.out.println("分布式锁释放成功...");
-            return JSON.parseObject(cacheJson, new TypeReference<Map<String, List<Category2Vo>>>(){});
         } else {
             // 加锁失败，休眠100ms后重试
-            System.out.println("获取分布式锁失败...等待重试");
+            //感觉还是有问题，竟然让如此多的线程在等待重试，为何不再其重试期间让其去查一下缓存呢
+            System.out.println("获取分布式锁失败...等待重试...");
             try {
-                Thread.sleep(100);
+                Thread.sleep(500);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
